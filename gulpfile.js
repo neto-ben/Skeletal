a06808812c065199bf4f37bcc46615446c57e2ad
Ry Van Veluwen <ry.vanveluwen@gmail.com>
2016-01-22 14:16:42 +1000

    Removing all styles from app.x and upgrading bootstrap in node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var less = require('gulp-sass');
 var path = require('path');
 
-gulp.task('less', function () {
-	gulp.src('./httpdocs/assets/themes/**/css/app.less')
+gulp.task('sass', function () {
+	gulp.src('./httpdocs/assets/themes/**/css/app.scss')
 	.pipe(less({
-		paths: [ path.join(__dirname, 'less', 'includes') ]
+		paths: [ path.join(__dirname, 'sass', 'includes') ]
 	}))
 	.pipe(gulp.dest('./httpdocs/assets/themes/'));
 });
 
 gulp.task('default', function () {
-	gulp.watch('./httpdocs/assets/themes/*/css/**/*.less', ['less']);
+	gulp.watch('./httpdocs/assets/themes/*/css/**/*.scss', ['sass']);
 });
